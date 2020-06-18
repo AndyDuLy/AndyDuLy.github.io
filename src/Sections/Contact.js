a52a9f6cc457b2334867b1c816082c57fe416d85
Andy Du Ly <andy.duly@benchedms.ca>
2020-06-17 20:03:16 -0400

    Contact form

--- src/Sections/Contact.js
+++ src/Sections/Contact.js
@@ -15,6 +15,27 @@ export default function Contact() {
       </div>
 
       <div className="row topMinorPadding">
+        <form name="contactForm" method="POST" data-netlify="true">
+          <div class="row">
+            <div class="six columns">
+              <label class="primaryText"> Full Name </label>
+              <input class="u-full-width" type="text" placeholder="John Doe"/>
+            </div>
+
+            <div class="six columns">
+              <label class="primaryText"> Email </label>
+              <input class="u-full-width" type="email" placeholder="john.doe@email.com"/>
+            </div>
+          </div>
+
+          <label class="primaryText"> Message </label>
+          <textarea class="u-full-width" placeholder="Message"/>
+
+          <input class="button-primary" type="submit" value="Submit"/>
+        </form>
+      </div>
+
+      <div className="row topMinorPadding">
         <div className="three columns">
           <a className="hyperlinkDecor" target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/andyduly">
             <h5 align="center" className="primaryText projectLink RalewayRegular"> LinkedIn </h5>
